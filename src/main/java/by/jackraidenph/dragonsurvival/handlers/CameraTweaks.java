f14debda55bb9c1c431d16b05f4f4725e43b5957
hohserg <hohserg1@yandex.ru>
2020-07-30 01:55:55 +0300

    Some stepts for camera motion

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/CameraTweaks.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/CameraTweaks.java
@@ -10,6 +10,7 @@ import net.minecraft.entity.EntityType;
 import net.minecraft.item.Item;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.IPacket;
+import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.EntityViewRenderEvent;
 import net.minecraftforge.event.RegistryEvent;
@@ -21,25 +22,51 @@ import org.lwjgl.opengl.GL11;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
+import static java.lang.Math.abs;
+
 @Mod.EventBusSubscriber(modid = DragonSurvivalMod.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE, value = Dist.CLIENT)
 public class CameraTweaks {
 
     private static Method movePosition;
+    private static Method setPosition;
 
     static {
         try {
             movePosition = ActiveRenderInfo.class.getDeclaredMethod("movePosition", double.class, double.class, double.class);
+            setPosition = ActiveRenderInfo.class.getDeclaredMethod("setPosition", double.class, double.class, double.class);
+
             movePosition.setAccessible(true);
+            setPosition.setAccessible(true);
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void setCamera(EntityViewRenderEvent.CameraSetup event, double dx, double dy, double dz, Vec3d lookVec) throws InvocationTargetException, IllegalAccessException {
+        ActiveRenderInfo info = event.getInfo();
+
+        movePosition.invoke(info, dx, dy, dz);
+        //setPosition.invoke(info, dx, dy, dz);
     }
 
     @SubscribeEvent
     public static void cameraSetup(final EntityViewRenderEvent.CameraSetup event) throws InvocationTargetException, IllegalAccessException, NoSuchMethodException {
         Minecraft minecraft = Minecraft.getInstance();
         ActiveRenderInfo info = event.getInfo();
-        movePosition.invoke(info, 0, 1, 0);
+
+        float pitch = event.getPitch();
+
+        //pitch=0 => dx=2, dy=0
+        //pitch=90 => dx=0, dy=2
+        //pitch=-90 => dx=0, dy=-2
+
+        int neckLen = 10;
+        double dx = (90 - abs(pitch)) / 90 * neckLen;
+        double dy = pitch / 90 * neckLen;
+
+        //System.out.println(event.getPitch());
+
+        setCamera(event, dx, dy+1, 0, null);
         //GL11.glTranslated(0, -1, 0);
 
     }
