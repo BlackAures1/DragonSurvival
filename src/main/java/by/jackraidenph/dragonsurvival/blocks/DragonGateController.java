e92eda87e12451792c2e5125fe7cb93c9bd19350
Alexiy <alexiy.ov@gmail.com>
2020-10-23 20:34:34 +0300

    Place gate blocks, and remove gate blocks

--- src/main/java/by/jackraidenph/dragonsurvival/blocks/DragonGateController.java
+++ src/main/java/by/jackraidenph/dragonsurvival/blocks/DragonGateController.java
@@ -1,9 +1,10 @@
 package by.jackraidenph.dragonsurvival.blocks;
 
+import by.jackraidenph.dragonsurvival.blockentities.DragonGateBlockEntity;
 import by.jackraidenph.dragonsurvival.handlers.BlockEntities;
+import by.jackraidenph.dragonsurvival.handlers.BlockInit;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.Direction;
@@ -30,17 +31,23 @@ public class DragonGateController extends DragonGateBlock {
     public void onBlockPlacedBy(World worldIn, BlockPos pos, BlockState state, LivingEntity placer, ItemStack stack) {
         super.onBlockPlacedBy(worldIn, pos, state, placer, stack);
         Direction direction=state.get(horizontal);
-        System.out.println(direction);
-    }
+        worldIn.setBlockState(pos.up(), BlockInit.dragonGate.getDefaultState().with(horizontal,direction));
+        worldIn.setBlockState(pos.up(2), BlockInit.dragonGate.getDefaultState().with(horizontal,direction));
+        worldIn.setBlockState(pos.offset(direction), BlockInit.dragonGate.getDefaultState().with(horizontal,direction));
+        worldIn.setBlockState(pos.offset(direction).up(), BlockInit.dragonGate.getDefaultState().with(horizontal,direction));
+        worldIn.setBlockState(pos.offset(direction).up(2), BlockInit.dragonGate.getDefaultState().with(horizontal,direction));
+        DragonGateBlockEntity dragonGateBlockEntity= (DragonGateBlockEntity) worldIn.getTileEntity(pos);
 
-    @Override
-    public BlockState getStateForPlacement(BlockItemUseContext context) {
-        return super.getStateForPlacement(context);
     }
 
     @Override
     public void onReplaced(BlockState state, World worldIn, BlockPos pos, BlockState newState, boolean isMoving) {
-
+        Direction direction=state.get(horizontal);
+        worldIn.removeBlock(pos.up(),false);
+        worldIn.removeBlock(pos.up(2),false);
+        worldIn.removeBlock(pos.offset(direction),false);
+        worldIn.removeBlock(pos.offset(direction).up(),false);
+        worldIn.removeBlock(pos.offset(direction).up(2),false);
         super.onReplaced(state, worldIn, pos, newState, isMoving);
     }
 }
