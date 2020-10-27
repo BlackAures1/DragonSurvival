59e56f94e0db20b85de44b2806ba581cc676f09a
Alexiy <alexiy.ov@gmail.com>
2020-10-27 16:59:05 +0200

    Move inside

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/BlocksInit.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/BlocksInit.java
@@ -3,14 +3,12 @@ package by.jackraidenph.dragonsurvival.handlers;
 import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
 import by.jackraidenph.dragonsurvival.blocks.DragonAltarBlock;
 import by.jackraidenph.dragonsurvival.blocks.PredatorStarBlock;
-import by.jackraidenph.dragonsurvival.tiles.PredatorStarTileEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.common.ToolType;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -19,26 +17,26 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = DragonSurvivalMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class BlocksInit {
 
-    public static final Block DRAGON_ALTAR_BLOCK = new DragonAltarBlock(
-            Block.Properties
-                    .create(Material.ANVIL)
-                    .harvestTool(ToolType.PICKAXE)
-                    .harvestLevel(2)
-                    .hardnessAndResistance(5.0f)
-                    .lightValue(5)
-                    .sound(SoundType.STONE));
+    public static Block DRAGON_ALTAR_BLOCK;
 
-    public static final Block PREDATOR_STAR_BLOCK = new PredatorStarBlock(
-            Block.Properties
-                    .create(Material.DRAGON_EGG)
-                    .doesNotBlockMovement()
-                    .hardnessAndResistance(9999)
-                    .tickRandomly()
-                    .noDrops()
-                    .sound(SoundType.NETHER_WART));
+    public static Block PREDATOR_STAR_BLOCK;
 
     @SubscribeEvent
     public static void registerBlocks(final RegistryEvent.Register<Block> event) {
+        DRAGON_ALTAR_BLOCK = new DragonAltarBlock(Block.Properties
+                        .create(Material.ANVIL)
+                        .harvestTool(ToolType.PICKAXE)
+                        .harvestLevel(2)
+                        .hardnessAndResistance(5.0f)
+                        .lightValue(5)
+                        .sound(SoundType.STONE));
+        PREDATOR_STAR_BLOCK = new PredatorStarBlock(Block.Properties
+                        .create(Material.DRAGON_EGG)
+                        .doesNotBlockMovement()
+                        .hardnessAndResistance(9999)
+                        .tickRandomly()
+                        .noDrops()
+                        .sound(SoundType.NETHER_WART));
         event.getRegistry().registerAll(
                 DRAGON_ALTAR_BLOCK,
                 PREDATOR_STAR_BLOCK);
