3c765d1c08cbef8c661ffea6a8e32141c4bf7095
Alexiy <alexiy.ov@gmail.com>
2020-10-27 17:02:35 +0200

    Move inside

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/TileEntityTypesInit.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/TileEntityTypesInit.java
@@ -10,13 +10,11 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = DragonSurvivalMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class TileEntityTypesInit {
 
-    public static final TileEntityType<PredatorStarTileEntity> PREDATOR_STAR_TILE_ENTITY_TYPE = TileEntityType.Builder.create(
-            PredatorStarTileEntity::new,
-            BlocksInit.PREDATOR_STAR_BLOCK)
-            .build(null);
+    public static TileEntityType<PredatorStarTileEntity> PREDATOR_STAR_TILE_ENTITY_TYPE;
 
     @SubscribeEvent
     public static void registerTE(RegistryEvent.Register<TileEntityType<?>> event) {
+        PREDATOR_STAR_TILE_ENTITY_TYPE = TileEntityType.Builder.create(PredatorStarTileEntity::new, BlocksInit.PREDATOR_STAR_BLOCK).build(null);
         event.getRegistry().registerAll(
                 PREDATOR_STAR_TILE_ENTITY_TYPE.setRegistryName(DragonSurvivalMod.MODID, "predator_star_te"));
     }
