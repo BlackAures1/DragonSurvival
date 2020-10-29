9aa10d58febfda6ae479a1099eaee6f3c637faf5
Alexiy <alexiy.ov@gmail.com>
2020-10-29 20:18:03 +0200

    Implemented render of body in first person; implemented body rotation change on movement

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/ClientEvents.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/ClientEvents.java
@@ -1,15 +1,23 @@
 package by.jackraidenph.dragonsurvival.handlers;
 
 import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
+import by.jackraidenph.dragonsurvival.capability.PlayerStateProvider;
 import by.jackraidenph.dragonsurvival.containers.DragonInventoryContainer;
 import by.jackraidenph.dragonsurvival.gui.DragonInventoryScreen;
 import by.jackraidenph.dragonsurvival.models.DragonModel;
+import com.mojang.blaze3d.matrix.MatrixStack;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.inventory.InventoryScreen;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.Vector3f;
+import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.client.event.RenderHandEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -17,7 +25,16 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(value = Dist.CLIENT)
 public class ClientEvents {
 
-    public static DragonModel<PlayerEntity> dragonModel=new DragonModel<>();
+    public static DragonModel<PlayerEntity> bodyAndLimbs = new DragonModel<>();
+    public static DragonModel<PlayerEntity> neckAndHead = new DragonModel<>();
+
+    public static float bodyYaw;
+
+    static {
+        neckAndHead.renderBody = false;
+        bodyAndLimbs.renderHead = false;
+        neckAndHead.Head.showModel = false;
+    }
 
     @SubscribeEvent
     public static void openPlayerInventory(GuiOpenEvent guiOpenEvent) {
@@ -30,7 +47,36 @@ public class ClientEvents {
 
     @SubscribeEvent
     public static void onRenderHand(RenderHandEvent renderHandEvent) {
-        if (DragonSurvivalMod.playerIsDragon(Minecraft.getInstance().player) && renderHandEvent.getItemStack().isEmpty())
-            renderHandEvent.setCanceled(true);
+        ClientPlayerEntity player = Minecraft.getInstance().player;
+
+        PlayerStateProvider.getCap(player).ifPresent(playerStateHandler -> {
+            if (renderHandEvent.getItemStack().isEmpty())
+                renderHandEvent.setCanceled(true);
+            MatrixStack matrixStack = renderHandEvent.getMatrixStack();
+            matrixStack.push();
+            bodyAndLimbs.player = player;
+            neckAndHead.player = player;
+            bodyAndLimbs.setRotationAngles(player, player.limbSwing, player.limbSwingAmount, player.ticksExisted, player.getYaw(renderHandEvent.getPartialTicks()), player.getPitch(renderHandEvent.getPartialTicks()));
+            neckAndHead.setRotationAngles(player, player.limbSwing, player.limbSwingAmount, player.ticksExisted, player.getYaw(renderHandEvent.getPartialTicks()), player.getPitch(renderHandEvent.getPartialTicks()));
+            String texture = "textures/dragon/" + playerStateHandler.getType().toString().toLowerCase() + ".png";
+            matrixStack.rotate(Vector3f.XP.rotationDegrees(player.rotationPitch));
+            matrixStack.rotate(Vector3f.YP.rotationDegrees(180));
+            matrixStack.rotate(Vector3f.YP.rotationDegrees(player.rotationYaw));
+            matrixStack.rotate(Vector3f.YP.rotationDegrees(-bodyYaw));
+            matrixStack.translate(0, -2, -1);
+            bodyAndLimbs.render(renderHandEvent.getMatrixStack(), renderHandEvent.getBuffers().getBuffer(RenderType.getEntityTranslucentCull(new ResourceLocation(DragonSurvivalMod.MODID, texture))), renderHandEvent.getLight(), LivingRenderer.getPackedOverlay(player, 0), renderHandEvent.getPartialTicks(), player.getYaw(renderHandEvent.getPartialTicks()), player.getPitch(renderHandEvent.getPartialTicks()), 1);
+            //TODO fix rotation
+            matrixStack.translate(0, 0, 0.15);
+            neckAndHead.render(renderHandEvent.getMatrixStack(), renderHandEvent.getBuffers().getBuffer(RenderType.getEntityTranslucentCull(new ResourceLocation(DragonSurvivalMod.MODID, texture))), renderHandEvent.getLight(), LivingRenderer.getPackedOverlay(player, 0), renderHandEvent.getPartialTicks(), player.getYaw(renderHandEvent.getPartialTicks()), player.getPitch(renderHandEvent.getPartialTicks()), 1);
+            matrixStack.pop();
+        });
+    }
+
+    @SubscribeEvent
+    public static void onKey(InputEvent.KeyInputEvent keyInputEvent) {
+        Minecraft minecraft = Minecraft.getInstance();
+        if (minecraft.player != null && (minecraft.gameSettings.keyBindForward.isPressed() || minecraft.gameSettings.keyBindLeft.isPressed() || minecraft.gameSettings.keyBindRight.isPressed() || minecraft.gameSettings.keyBindBack.isPressed())) {
+            bodyYaw = minecraft.player.rotationYaw;
+        }
     }
 }
