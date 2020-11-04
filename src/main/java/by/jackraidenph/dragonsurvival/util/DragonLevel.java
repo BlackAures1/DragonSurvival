707693ef12f0b86df0c38032c9c4585839987601
Alexiy <alexiy.ov@gmail.com>
2020-11-04 10:27:00 +0200

    Added dragon levels

---
+++ src/main/java/by/jackraidenph/dragonsurvival/util/DragonLevel.java
@@ -0,0 +1,19 @@
+package by.jackraidenph.dragonsurvival.util;
+
+public enum DragonLevel {
+    BABY(7, 1.1f, 0.4f, 0.9f, 0.9f),
+    YOUNG(10, 1.6f, 0.9f, 1.9f, 1.9f),
+    ADULT(15, 2.1f, 1.4f, 2.3f, 2.3f);
+
+    public int initialHealth;
+    public float jumpHeight;
+    public float maxWidth, maxHeight, maxLength;
+
+    DragonLevel(int initialHealth, float jumpHeight, float maxWidth, float maxHeight, float maxLength) {
+        this.initialHealth = initialHealth;
+        this.jumpHeight = jumpHeight;
+        this.maxWidth = maxWidth;
+        this.maxHeight = maxHeight;
+        this.maxLength = maxLength;
+    }
+}
