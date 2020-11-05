fa88bd02b4fcb5eab09283c5d934f01a78cbdbcd
Alexiy <alexiy.ov@gmail.com>
2020-11-05 14:52:31 +0200

    Moved client setup event

--- src/main/java/by/jackraidenph/dragonsurvival/DragonSurvivalMod.java
+++ src/main/java/by/jackraidenph/dragonsurvival/DragonSurvivalMod.java
@@ -3,25 +3,15 @@ package by.jackraidenph.dragonsurvival;
 import by.jackraidenph.dragonsurvival.capability.DragonStateHandler;
 import by.jackraidenph.dragonsurvival.capability.PlayerStateCapability;
 import by.jackraidenph.dragonsurvival.capability.PlayerStateProvider;
-import by.jackraidenph.dragonsurvival.handlers.BlockInit;
 import by.jackraidenph.dragonsurvival.handlers.EntityTypesInit;
-import by.jackraidenph.dragonsurvival.handlers.TileEntityTypesInit;
 import by.jackraidenph.dragonsurvival.network.*;
-import by.jackraidenph.dragonsurvival.renderer.MagicalPredatorRenderer;
-import by.jackraidenph.dragonsurvival.renderer.PredatorStarTESR;
-import by.jackraidenph.dragonsurvival.shader.ShaderHelper;
 import by.jackraidenph.dragonsurvival.util.ConfigurationHandler;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.client.registry.ClientRegistry;
-import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.NetworkRegistry;
@@ -45,7 +35,6 @@ public class DragonSurvivalMod {
 
     public DragonSurvivalMod() {
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setupClient);
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ConfigurationHandler.SPEC);
         MinecraftForge.EVENT_BUS.register(this);
     }
@@ -70,12 +59,5 @@ public class DragonSurvivalMod {
         LOGGER.info("Successfully registered Messages!");
         EntityTypesInit.addSpawn();
         LOGGER.info("Successfully registered Entity Spawns!");
-    }
-
-    private void setupClient(final FMLClientSetupEvent event) {
-        RenderTypeLookup.setRenderLayer(BlockInit.dragon_altar, RenderType.getTranslucent());
-        RenderingRegistry.registerEntityRenderingHandler(EntityTypesInit.MAGICAL_BEAST, MagicalPredatorRenderer::new);
-        ClientRegistry.bindTileEntityRenderer(TileEntityTypesInit.PREDATOR_STAR_TILE_ENTITY_TYPE, PredatorStarTESR::new);
-        ShaderHelper.initShaders();
     }
 }
--- src/main/java/by/jackraidenph/dragonsurvival/handlers/ClientModEvents.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/ClientModEvents.java
@@ -1,10 +1,18 @@
 package by.jackraidenph.dragonsurvival.handlers;
 
 import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
+import by.jackraidenph.dragonsurvival.renderer.MagicalPredatorRenderer;
+import by.jackraidenph.dragonsurvival.renderer.PredatorStarTESR;
+import by.jackraidenph.dragonsurvival.shader.ShaderHelper;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.TextureStitchEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientModEvents {
@@ -15,5 +23,13 @@ public class ClientModEvents {
         event.addSprite(new ResourceLocation(DragonSurvivalMod.MODID, "te/star/open_eye"));
         event.addSprite(new ResourceLocation(DragonSurvivalMod.MODID, "te/star/wind_vertical"));
         DragonSurvivalMod.LOGGER.info("Successfully added sprites!");
+    }
+
+    @SubscribeEvent
+    public static void setupClient(final FMLClientSetupEvent event) {
+        RenderTypeLookup.setRenderLayer(BlockInit.dragon_altar, RenderType.getCutout());
+        RenderingRegistry.registerEntityRenderingHandler(EntityTypesInit.MAGICAL_BEAST, MagicalPredatorRenderer::new);
+        ClientRegistry.bindTileEntityRenderer(TileEntityTypesInit.PREDATOR_STAR_TILE_ENTITY_TYPE, PredatorStarTESR::new);
+        ShaderHelper.initShaders();
     }
 }
