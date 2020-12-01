c441144f35b40d2ccde39dddcc3c8eb02e594df6
Alexiy <alexiy.ov@gmail.com>
2020-12-01 13:06:01 +0200

    Fix (server compat.)

--- src/main/java/by/jackraidenph/dragonsurvival/entity/MagicalPredatorEntity.java
+++ src/main/java/by/jackraidenph/dragonsurvival/entity/MagicalPredatorEntity.java
@@ -4,7 +4,6 @@ import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
 import by.jackraidenph.dragonsurvival.capability.PlayerStateProvider;
 import by.jackraidenph.dragonsurvival.handlers.BlockInit;
 import by.jackraidenph.dragonsurvival.network.PacketSyncXPDevour;
-import by.jackraidenph.dragonsurvival.renderer.MagicalPredatorRenderer;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.*;
 import net.minecraft.entity.ai.attributes.AttributeModifier;
@@ -39,7 +38,7 @@ public class MagicalPredatorEntity extends MonsterEntity {
 
     public MagicalPredatorEntity(EntityType<? extends MonsterEntity> entityIn, World worldIn) {
         super(entityIn, worldIn);
-        this.type = worldIn.getRandom().nextInt(MagicalPredatorRenderer.MAGICAL_BEAST_TEXTURES.size());
+        this.type = worldIn.getRandom().nextInt(10);
         this.size = worldIn.getRandom().nextFloat() + 0.95F;
         scale = this.size / this.getHeight();
     }
