64b305112b6a381c6659b26ab77617c58c9cd82e
alexiy <alexiy.ov@gmail.com>
2021-02-06 14:05:49 +0200

    Original elytra code

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/FlightController.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/FlightController.java
@@ -1,10 +1,13 @@
 package by.jackraidenph.dragonsurvival.handlers;
 
-import by.jackraidenph.dragonsurvival.Functions;
 import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.MoverType;
+import net.minecraft.entity.ai.attributes.IAttributeInstance;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.InputEvent;
@@ -16,64 +19,61 @@ import net.minecraftforge.fml.common.Mod;
 @SuppressWarnings("unused")
 public class FlightController {
 
-    static double velX;
-    static double velY;
-    static double velZ;
-
     static boolean wingsEnabled;
 
     @SubscribeEvent
-    public static void controlVelocity(TickEvent.PlayerTickEvent playerTickEvent) {
+    public static void flightControl(TickEvent.PlayerTickEvent playerTickEvent) {
         PlayerEntity playerEntity = playerTickEvent.player;
         DragonStateProvider.getCap(playerEntity).ifPresent(dragonStateHandler -> {
             if (dragonStateHandler.isDragon()) {
-                if (!playerEntity.onGround && wingsEnabled) {
-                    //my code
-                    double verticalSpeed = playerEntity.rotationPitch;
-                    float radians = Math.min(Functions.degreesToRadians(30) + 90, Functions.degreesToRadians(playerEntity.rotationYawHead + 90));
-                    if (verticalSpeed < 0) {
-                        playerEntity.addVelocity(MathHelper.cos(radians) / 10, .1, MathHelper.sin(radians) / 10);
-                    } else {
-                        playerEntity.addVelocity(MathHelper.cos(radians) / 16, 0, MathHelper.sin(radians) / 16);
-                    }
+                if (!playerEntity.onGround && !playerEntity.isInWater() && wingsEnabled) {
 
-                    //original code
-//                    double yaw = Functions.degreesToRadians(playerEntity.rotationYawHead);
-//                    double pitch = Functions.degreesToRadians(playerEntity.rotationPitch);
-//                    double yawcos = Math.cos(-yaw - Math.PI);
-//                    double yawsin = Math.sin(-yaw - Math.PI);
-//                    double pitchcos = Math.cos(pitch);
-//                    double pitchsin = Math.sin(pitch);
+                    {
+                        Vec3d vec3d3 = playerEntity.getMotion();
+                        if (vec3d3.y > -0.5D) {
+                            playerEntity.fallDistance = 1.0F;
+                        }
+
+                        Vec3d vec3d = playerEntity.getLookVec();
+                        float f6 = playerEntity.rotationPitch * ((float) Math.PI / 180F);
+                        double d9 = Math.sqrt(vec3d.x * vec3d.x + vec3d.z * vec3d.z);
+                        double d11 = Math.sqrt(LivingEntity.horizontalMag(vec3d3));
+                        double d12 = vec3d.length();
+                        float f3 = MathHelper.cos(f6);
+                        f3 = (float) ((double) f3 * (double) f3 * Math.min(1.0D, d12 / 0.4D));
+                        IAttributeInstance gravity = playerEntity.getAttribute(LivingEntity.ENTITY_GRAVITY);
+                        double g = gravity.getValue();
+                        vec3d3 = playerEntity.getMotion().add(0.0D, g * (-1.0D + (double) f3 * 0.75D), 0.0D);
+                        if (vec3d3.y < 0.0D && d9 > 0.0D) {
+                            double d3 = vec3d3.y * -0.1D * (double) f3;
+                            vec3d3 = vec3d3.add(vec3d.x * d3 / d9, d3, vec3d.z * d3 / d9);
+                        }
+
+                        if (f6 < 0.0F && d9 > 0.0D) {
+                            double d13 = d11 * (double) (-MathHelper.sin(f6)) * 0.04D;
+                            vec3d3 = vec3d3.add(-vec3d.x * d13 / d9, d13 * 3.2D, -vec3d.z * d13 / d9);
+                        }
+
+                        if (d9 > 0.0D) {
+                            vec3d3 = vec3d3.add((vec3d.x / d9 * d11 - vec3d3.x) * 0.1D, 0.0D, (vec3d.z / d9 * d11 - vec3d3.z) * 0.1D);
+                        }
+
+                        playerEntity.setMotion(vec3d3.mul(0.99F, 0.98F, 0.99F));
+                        playerEntity.move(MoverType.SELF, playerEntity.getMotion());
+//                        if (playerEntity.collidedHorizontally && !playerEntity.world.isRemote) {
+//                            double d14 = Math.sqrt(LivingEntity.horizontalMag(playerEntity.getMotion()));
+//                            double d4 = d11 - d14;
+//                            float f4 = (float)(d4 * 10.0D - 3.0D);
+//                            if (f4 > 0.0F) {
+//                                playerEntity.playSound(playerEntity.getFallSound((int)f4), 1.0F, 1.0F);
+//                                playerEntity.attackEntityFrom(DamageSource.FLY_INTO_WALL, f4);
+//                            }
+//                        }
 //
-//                    double lookX = yawsin * -pitchcos;
-//                    double lookZ = yawcos * -pitchcos;
-//
-//                    double hvel = Math.sqrt(velX * velX + velZ * velZ);
-//                    double sqrpitchcos = pitchcos * pitchcos;
-//
-//                    velY += -0.08 + sqrpitchcos * 0.06;
-//
-//                    if (velY < 0 && pitchcos > 0) {
-//                        double yacc = velY * -0.1 * sqrpitchcos;
-//                        velY += yacc;
-//                        velX += lookX * yacc / pitchcos;
-//                        velZ += lookZ * yacc / pitchcos;
-//                    }
-//                    if (pitch < 0) {
-//                        double yacc = hvel * -pitchsin * 0.04;
-//                        velY += yacc * 3.5;
-//                        velX -= lookX * yacc / pitchcos;
-//                        velZ -= lookZ * yacc / pitchcos;
-//                    }
-//                    if (pitchcos > 0) {
-//                        velX += (lookX / pitchcos * hvel - velX) * 0.1;
-//                        velZ += (lookZ / pitchcos * hvel - velZ) * 0.1;
-//                    }
-//
-//                    velX *= 0.99;
-//                    velY *= 0.98;
-//                    velZ *= 0.99;
-//                    playerEntity.addVelocity(MathHelper.clamp( velX,velX/5,velX*0.6), MathHelper.clamp(velY, 0, 0.7), MathHelper.clamp(velZ,velZ/5,velZ*0.6));
+//                        if (playerEntity.onGround && !playerEntity.world.isRemote) {
+//                            playerEntity.setFlag(7, false);
+//                        }
+                    }
                 }
             }
         });
