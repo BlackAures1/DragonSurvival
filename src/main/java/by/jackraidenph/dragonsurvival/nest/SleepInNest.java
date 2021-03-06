e0a6be3641a025f27888dbd0870b431d3cb4e1e7
alexiy <alexiy.ov@gmail.com>
2021-03-06 18:01:51 +0200

    Sleep packet

---
+++ src/main/java/by/jackraidenph/dragonsurvival/nest/SleepInNest.java
@@ -0,0 +1,15 @@
+package by.jackraidenph.dragonsurvival.nest;
+
+import net.minecraft.util.math.BlockPos;
+
+public class SleepInNest {
+    public SleepInNest() {
+    }
+
+    public BlockPos nestPos;
+
+    public SleepInNest(BlockPos nestPos) {
+        this.nestPos = nestPos;
+    }
+
+}
