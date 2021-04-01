20c3a8b3b8a529b74c6a0b7fdaca0374a4e55c62
alexiy <alexiy.ov@gmail.com>
2021-04-01 13:10:45 +0300

    Added most of animations

--- src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonEntity.java
+++ src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonEntity.java
@@ -3,10 +3,12 @@ package by.jackraidenph.dragonsurvival.gecko;
 import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.Pose;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EquipmentSlotType;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.HandSide;
+import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import software.bernie.geckolib3.core.IAnimatable;
 import software.bernie.geckolib3.core.PlayState;
@@ -31,7 +33,7 @@ public class DragonEntity extends LivingEntity implements IAnimatable {
 
     @Override
     public void registerControllers(AnimationData animationData) {
-        animationData.addAnimationController(new AnimationController<>(this, "controller", 0, this::predicate));
+        animationData.addAnimationController(new AnimationController<>(this, "controller", 10, this::predicate));
     }
 
     @Override
@@ -40,12 +42,27 @@ public class DragonEntity extends LivingEntity implements IAnimatable {
     }
 
     private <E extends IAnimatable> PlayState predicate(AnimationEvent<E> animationEvent) {
-        if (!player.onGround && !player.isInWater() && player.getCapability(DragonStateProvider.DRAGON_CAPABILITY).orElse(null).hasWings()) {
+        Vec3d motio = player.getMotion();
+        if (player.getPose() == Pose.SWIMMING)
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.swim_fast", true));
+        else if (player.isInWaterOrBubbleColumn() && (motio.x != 0 || motio.z != 0)) {
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.swim", true));
+        } else if (player.getMotion().y > 0)
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.model.new2"));
+        else if (!player.onGround && !player.isInWater() && player.getCapability(DragonStateProvider.DRAGON_CAPABILITY).orElse(null).hasWings()) {
             animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.fly_slow", true));
-        } else if (player.isSleeping()) {
+        } else if (player.isSprinting())
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.run", true));
+        else if (player.isSneaking())
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.stand5", true));
+        else if (player.getCooledAttackStrength(0.5f) != 1)
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.model.new"));
+        else if (motio.getX() != 0 || motio.getZ() != 0)
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.stand3", true));
+        else if (player.isSleeping()) {
             animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.sleep", true));
         } else
-            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.stand2", true));
+            animationEvent.getController().setAnimation(new AnimationBuilder().addAnimation("animation.dragon.stand3", true));
         return PlayState.CONTINUE;
     }
 
--- src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonModel.java
+++ src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonModel.java
@@ -2,7 +2,9 @@ package by.jackraidenph.dragonsurvival.gecko;
 
 import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
 import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
+import net.minecraft.entity.Pose;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.Vec3d;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 
 public class DragonModel extends AnimatedGeoModel<DragonEntity> {
@@ -25,8 +27,27 @@ public class DragonModel extends AnimatedGeoModel<DragonEntity> {
 
     @Override
     public ResourceLocation getAnimationFileLocation(DragonEntity dragonEntity) {
+        Vec3d vec3d = dragonEntity.player.getMotion();
+        if (dragonEntity.player.getPose() == Pose.SWIMMING)
+            return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.swim_fast.animation.json");
+        if (dragonEntity.player.isInWaterOrBubbleColumn() && (vec3d.x != 0 || vec3d.z != 0))
+            return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.swim.animation.json");
+        if (dragonEntity.player.isSneaking()) {
+            return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.sneaking.animation.json");
+        }
+        if (dragonEntity.player.getMotion().y > 0)
+            return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.jump.animation.json");
+        //TODO only when wings are active
         if (!dragonEntity.player.isInWater() && !dragonEntity.player.onGround && dragonEntity.player.getCapability(DragonStateProvider.DRAGON_CAPABILITY).orElse(null).hasWings())
             return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.fly.animation.json");
+
+        if (dragonEntity.player.isSprinting())
+            return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.run.animation.json");
+        if (dragonEntity.player.getCooledAttackStrength(0.5f) != 1) {
+            return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.bite.animation.json");
+        }
+        if (vec3d.z != 0 || vec3d.x != 0)
+            return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.walk.animation.json");
         if (dragonEntity.player.isSleeping())
             return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.sleep.animation.json");
         return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon.stand.animation.json");
