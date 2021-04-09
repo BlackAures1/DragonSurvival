a187fa9be777bdb81ffc6c5f78be0964bc1f1f54
alexiy <alexiy.ov@gmail.com>
2021-04-09 11:17:06 +0300

    Loop the jump animation

--- src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonEntity.java
+++ src/main/java/by/jackraidenph/dragonsurvival/gecko/DragonEntity.java
@@ -67,7 +67,7 @@ public class DragonEntity extends LivingEntity implements IAnimatable {
         else if (player.isSwingInProgress && player.getCooledAttackStrength(-3.0f) != 1)
             animationController.setAnimation(new AnimationBuilder().addAnimation("animation.model.new"));
         else if (ClientEvents.dragonsJumpingTicks.getOrDefault(player, 0) > 0)
-            animationController.setAnimation(new AnimationBuilder().addAnimation("animation.model.new2", false));
+            animationController.setAnimation(new AnimationBuilder().addAnimation("animation.model.new2", true));
         else if ((motio.getX() != 0 || motio.getZ() != 0) && player.limbSwingAmount > 0.1f)
             animationController.setAnimation(new AnimationBuilder().addAnimation("animation.dragon.stand3", true));
         else if (player.isSleeping()) {
