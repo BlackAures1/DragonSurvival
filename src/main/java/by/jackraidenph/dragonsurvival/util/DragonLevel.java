d94c054aadc9dd29f43bfe14a21c010f30071d50
alexiy <alexiy.ov@gmail.com>
2021-04-25 12:48:35 +0300

    Added base damages

--- src/main/java/by/jackraidenph/dragonsurvival/util/DragonLevel.java
+++ src/main/java/by/jackraidenph/dragonsurvival/util/DragonLevel.java
@@ -1,21 +1,23 @@
 package by.jackraidenph.dragonsurvival.util;
 
 public enum DragonLevel {
-    BABY(14, 1.1f, 0.4f, 0.9f, 0.9f, "newborn"),
-    YOUNG(20, 1.6f, 0.9f, 1.9f, 1.9f, "young"),
-    ADULT(30, 2.1f, 1.4f, 2.3f, 2.3f, "adult");
+    BABY(14, 1.1f, 0.4f, 0.9f, 0.9f, "newborn", 2),
+    YOUNG(20, 1.6f, 0.9f, 1.9f, 1.9f, "young", 4),
+    ADULT(30, 2.1f, 1.4f, 2.3f, 2.3f, "adult", 6);
 
     public int initialHealth;
     public float jumpHeight;
     public float maxWidth, maxHeight, maxLength;
     public String name;
+    public int baseDamage;
 
-    DragonLevel(int initialHealth, float jumpHeight, float maxWidth, float maxHeight, float maxLength, String name_) {
+    DragonLevel(int initialHealth, float jumpHeight, float maxWidth, float maxHeight, float maxLength, String name_, int baseDamage) {
         this.initialHealth = initialHealth;
         this.jumpHeight = jumpHeight;
         this.maxWidth = maxWidth;
         this.maxHeight = maxHeight;
         this.maxLength = maxLength;
         this.name = name_;
+        this.baseDamage = baseDamage;
     }
 }
