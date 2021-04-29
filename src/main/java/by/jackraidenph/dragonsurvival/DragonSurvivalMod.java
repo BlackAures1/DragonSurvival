942dd37b0a341354b996f7b195029d83d32d0798
alexiy <alexiy.ov@gmail.com>
2021-04-29 18:11:37 +0300

    Perm. level 2

--- src/main/java/by/jackraidenph/dragonsurvival/DragonSurvivalMod.java
+++ src/main/java/by/jackraidenph/dragonsurvival/DragonSurvivalMod.java
@@ -275,7 +275,7 @@ public class DragonSurvivalMod {
     private void onServerStart(FMLServerStartingEvent serverStartingEvent) {
         CommandDispatcher<CommandSource> commandDispatcher = serverStartingEvent.getCommandDispatcher();
         RootCommandNode<CommandSource> rootCommandNode = commandDispatcher.getRoot();
-        LiteralCommandNode<CommandSource> dragon = literal("dragon").requires(commandSource -> commandSource.hasPermissionLevel(4)).build();
+        LiteralCommandNode<CommandSource> dragon = literal("dragon").requires(commandSource -> commandSource.hasPermissionLevel(2)).build();
 
         ArgumentCommandNode<CommandSource, String> dragonType = argument("dragon_type", StringArgumentType.string()).suggests((context, builder) -> ISuggestionProvider.suggest(new String[]{"cave", "sea", "forest"}, builder)).build();
 
