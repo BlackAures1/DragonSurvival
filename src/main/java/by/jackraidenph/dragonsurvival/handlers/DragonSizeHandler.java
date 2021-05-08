fdfedb7a0bdedc9a2533b82c63e4d612360b4221
Arcaryx <arcaryx@gmail.com>
2021-05-08 02:15:57 -0400

    Fixed server flight hitbox and swimming hitbox desync

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/DragonSizeHandler.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/DragonSizeHandler.java
@@ -1,6 +1,7 @@
 package by.jackraidenph.dragonsurvival.handlers;
 
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 
 import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
 import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
@@ -11,6 +12,7 @@ import net.minecraft.entity.Pose;
 import net.minecraft.entity.ai.attributes.Attributes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.event.TickEvent;
@@ -35,6 +37,7 @@ public class DragonSizeHandler {
     		float eyeHeight = calculateDragonEyeHeight(health, ConfigurationHandler.hitboxGrowsPastHuman.get());
     		// Handle Pose stuff
     		Pose overridePose = overridePose(player);
+    		player.sendMessage(new StringTextComponent(("Tick1 for " + player.getDisplayName().getString() + ": hp(" + health + "), p(" + overridePose.toString() + ")")), player.getUUID());
     		height = calculateModifiedHeight(height, overridePose);
     		eyeHeight = calculateModifiedEyeHeight(eyeHeight, overridePose);
     		// Apply changes
@@ -96,48 +99,56 @@ public class DragonSizeHandler {
     
     private static Pose overridePose(PlayerEntity player) {
     	Pose overridePose = getOverridePose(player);
-    	if (player.getForcedPose() != overridePose)
-			player.setForcedPose(overridePose);
+    	if (player.getForcedPose() != overridePose) {
+    		player.setForcedPose(overridePose);
+    		player.sendMessage(new StringTextComponent(("Tick2 for " + player.getDisplayName().getString() + ": p(" + overridePose.toString() + ")")), player.getUUID());
+    		if (player.level.isClientSide() && Minecraft.getInstance().cameraEntity != player)
+    			player.refreshDimensions();
+    	}
+			
     	return overridePose;
     }
     
+    // Server Only
+    public static ConcurrentHashMap<Integer, Boolean> serverWingsEnabled = new ConcurrentHashMap<>(20);
+    
     private static Pose getOverridePose(PlayerEntity player) {
     	boolean swimming = player.isInWaterOrBubble() && player.isAffectedByFluids(); // TODO This needs more work to avoid bobbing
-		boolean flying = !player.isOnGround() && !player.isInWater() && FlightController.wingsEnabled && player.getCapability(DragonStateProvider.DRAGON_CAPABILITY).orElse(null).hasWings() ;
-		boolean spinning = player.isAutoSpinAttack();
-		boolean crouching = player.isShiftKeyDown(); // TODO This might need more work
+    	boolean flying = (player.level.isClientSide && ClientEvents.dragonsFlying.getOrDefault(player.getId(), false) && !player.isInWater() && !player.isOnGround() && player.getCapability(DragonStateProvider.DRAGON_CAPABILITY).orElse(null).hasWings())
+				|| (!player.level.isClientSide && !player.isOnGround() && serverWingsEnabled.getOrDefault(player.getId(), false) && !player.isInWater() && player.getCapability(DragonStateProvider.DRAGON_CAPABILITY).orElse(null).hasWings());
+    	boolean spinning = player.isAutoSpinAttack();
+		boolean crouching = player.isShiftKeyDown();
 		if (flying)
 			return Pose.FALL_FLYING;
 		else if (swimming)
 			return Pose.SWIMMING;
 		else if (spinning)
 			return Pose.SPIN_ATTACK;
-		else {
-			if (crouching || (!canPoseFit(player, Pose.STANDING) && canPoseFit(player, Pose.CROUCHING)))
+		else if (crouching || (!canPoseFit(player, Pose.STANDING) && canPoseFit(player, Pose.CROUCHING)))
 				return Pose.CROUCHING;
-		}
 		return Pose.STANDING;
     }
     
-    private static HashMap<Integer, Boolean> wasDragon = new HashMap<Integer, Boolean>();
+    private static ConcurrentHashMap<Integer, Boolean> wasDragon = new ConcurrentHashMap<Integer, Boolean>(20);
     
     @SubscribeEvent
-    @OnlyIn(Dist.CLIENT)
     public static void playerTick(TickEvent.PlayerTickEvent event) {
-    	if (event.player == null || event.phase == TickEvent.Phase.END || Minecraft.getInstance().getCameraEntity() != event.player)
+    	PlayerEntity player = event.player;
+    	if (player == null || event.phase == TickEvent.Phase.END)
     		return;
-    	DragonStateProvider.getCap(event.player).ifPresent(dragonStateHandler -> {
+    	DragonStateProvider.getCap(player).ifPresent(dragonStateHandler -> {
     		if (dragonStateHandler.isDragon()) {
-    			overridePose(event.player);
-    			event.player.refreshDimensions(); // FIXME Yowch is there a way to implement this without running it every tick?
-    			if (!wasDragon.getOrDefault(event.player.getId(), false)) {
-    				wasDragon.put(event.player.getId(), true);
-    				
+    			Pose pose = overridePose(player);
+    			if (!wasDragon.getOrDefault(player.getId(), false)) {
+    				player.refreshDimensions();
+    				player.sendMessage(new StringTextComponent(("Tick3 for " + player.getDisplayName().getString() + ": p(" + pose.toString() + ")")), player.getUUID());
+    				wasDragon.put(player.getId(), true);
     			}
-    		} else if (wasDragon.getOrDefault(event.player.getId(), false)) {
-				event.player.setForcedPose(null);
-				event.player.refreshDimensions();
-				wasDragon.put(event.player.getId(), false);
+    		} else if (wasDragon.getOrDefault(player.getId(), false)) {
+    			player.setForcedPose(null);
+    			player.refreshDimensions();
+    			player.sendMessage(new StringTextComponent(("Tick4 for " + player.getDisplayName().getString())), player.getUUID());
+    			wasDragon.put(player.getId(), false);
 			}
     	});
     }
