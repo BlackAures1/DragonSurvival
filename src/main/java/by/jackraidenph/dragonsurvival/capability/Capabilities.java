4a634d25a3d4f465a49959678834fef8c4936dd7
alexiy <alexiy.ov@gmail.com>
2021-05-12 10:56:27 +0300

    Rename

--- src/main/java/by/jackraidenph/dragonsurvival/capability/Capabilities.java
+++ src/main/java/by/jackraidenph/dragonsurvival/capability/Capabilities.java
@@ -4,6 +4,6 @@ import net.minecraftforge.common.capabilities.CapabilityManager;
 
 public class Capabilities {
     public static void register() {
-        CapabilityManager.INSTANCE.register(DragonStateHandler.class, new CapabilityStorage(), DragonStateHandler::new);
+        CapabilityManager.INSTANCE.register(DragonStateHandler.class, new DragonCapStorage(), DragonStateHandler::new);
     }
 }
