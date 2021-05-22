940a25cac2d54fb48befc01bb6e157d1708eaf27
Arcaryx <arcaryx@gmail.com>
2021-05-21 22:14:58 -0400

    Added dragon food HUD.

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/DragonFoodHandler.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/DragonFoodHandler.java
@@ -3,16 +3,25 @@ package by.jackraidenph.dragonsurvival.handlers;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.stream.Stream;
 
 import javax.annotation.Nullable;
 
+import org.lwjgl.opengl.GL11;
+
+import com.mojang.blaze3d.platform.GlStateManager;
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.datafixers.util.Pair;
 
 import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
 import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
 import by.jackraidenph.dragonsurvival.config.ConfigHandler;
 import by.jackraidenph.dragonsurvival.util.DragonType;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.gui.AbstractGui;
+import net.minecraft.client.gui.IngameGui;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
@@ -26,9 +35,12 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.FoodStats;
 import net.minecraft.util.Hand;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
+import net.minecraftforge.client.gui.ForgeIngameGui;
 import net.minecraftforge.event.entity.living.LivingEntityUseItemEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -38,9 +50,19 @@ import net.minecraftforge.fml.config.ModConfig.Type;
 import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = DragonSurvivalMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
-public class DragonFoodHandler {
+public class DragonFoodHandler{
 
 	private static Map<DragonType, Map<Item, Pair<Integer, Integer>>> DRAGON_FOODS;
+	
+	private final Minecraft mc;
+	private final ResourceLocation FOOD_ICONS;
+	private final Random rand;
+	
+	public DragonFoodHandler() {
+		mc = Minecraft.getInstance();
+		rand = new Random();
+		FOOD_ICONS = new ResourceLocation(DragonSurvivalMod.MODID + ":textures/gui/dragon_foods.png");
+	}
 	
 	@SubscribeEvent
 	public static void onConfigLoad(ModConfig.Loading event) {
@@ -214,6 +236,60 @@ public class DragonFoodHandler {
 			}
 		});
 	}
+	
+	@SubscribeEvent
+	public void onRenderFoodBar(RenderGameOverlayEvent.Pre event) {
+		ClientPlayerEntity player = this.mc.player;
+		
+		if (event.getType() != RenderGameOverlayEvent.ElementType.FOOD || player.isCreative() || player.isSpectator())
+			return;
+		DragonStateProvider.getCap(player).ifPresent(dragonStateHandler -> {
+			if (dragonStateHandler.isDragon()) {
+				event.setCanceled(true);
+				
+				rand.setSeed((long)(player.tickCount * 312871));
+				
+				RenderSystem.enableBlend();
+				this.mc.getTextureManager().bind(FOOD_ICONS);
+				
+				final int right_height = ForgeIngameGui.right_height;
+				ForgeIngameGui.right_height += 10;
+				
+				final int left = this.mc.getWindow().getGuiScaledWidth() / 2 + 91;
+                final int top = this.mc.getWindow().getGuiScaledHeight() - right_height;
+                
+                final FoodStats food = player.getFoodData();
+
+                final int type = dragonStateHandler.getType() == DragonType.FOREST ? 0 : dragonStateHandler.getType() == DragonType.CAVE ? 9 : 18;
+                
+                final boolean hunger = player.hasEffect(Effects.HUNGER);
+                
+                for (int i = 0; i < 10; ++i) {
+                	int idx = i * 2 + 1;
+                	int y = top;
+                	
+                	if (food.getSaturationLevel() <= 0.0F && player.tickCount % (food.getFoodLevel() * 3 + 1) == 0)
+                		y = top + (rand.nextInt(3) - 1);
+                    
+                	
+                	
+                	mc.gui.blit(event.getMatrixStack(), left - i * 8 - 9, y, (hunger ? 117 : 0), type, 9, 9);
+                	
+                	if (idx < food.getFoodLevel())
+                		mc.gui.blit(event.getMatrixStack(), left - i * 8 - 9, y, (hunger ? 72 : 36), type, 9, 9);
+                	else if (idx == food.getFoodLevel())
+                		mc.gui.blit(event.getMatrixStack(), left - i * 8 - 9, y, (hunger ? 81 : 45), type, 9, 9);
+                }
+                
+        		this.mc.getTextureManager().bind(AbstractGui.GUI_ICONS_LOCATION);
+        		RenderSystem.disableBlend();
+				
+                
+                
+			}
+		});
+	}
+	
 	
 	
 	
