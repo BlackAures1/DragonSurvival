14d698f373268645e325186a5c39dfcce54f13b6
Alexiy <alexiy.ov@gmail.com>
2021-09-17 17:41:22 +0300

    Add fluid check function

--- src/main/java/by/jackraidenph/dragonsurvival/nest/NestBlock.java
+++ src/main/java/by/jackraidenph/dragonsurvival/nest/NestBlock.java
@@ -27,7 +27,6 @@ import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.shapes.VoxelShapes;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.IBlockReader;
-import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.network.NetworkHooks;
 
