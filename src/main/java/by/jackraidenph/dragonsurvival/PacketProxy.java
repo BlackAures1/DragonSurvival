ef9b327f1395ed1b5f16293f47ceb2322732c48c
Alexiy <alexiy.ov@gmail.com>
2021-09-17 04:22:59 +0300

    Moved everything from ClientProxy to PacketProxy

--- src/main/java/by/jackraidenph/dragonsurvival/PacketProxy.java
+++ src/main/java/by/jackraidenph/dragonsurvival/PacketProxy.java
@@ -1,18 +1,24 @@
 package by.jackraidenph.dragonsurvival;
 
 import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
+import by.jackraidenph.dragonsurvival.entity.MagicalPredatorEntity;
 import by.jackraidenph.dragonsurvival.gecko.DragonEntity;
 import by.jackraidenph.dragonsurvival.handlers.ClientEvents;
 import by.jackraidenph.dragonsurvival.handlers.ClientFlightHandler;
 import by.jackraidenph.dragonsurvival.handlers.EntityTypesInit;
-import by.jackraidenph.dragonsurvival.network.PacketSyncCapabilityMovement;
-import by.jackraidenph.dragonsurvival.network.SyncCapabilityDebuff;
-import by.jackraidenph.dragonsurvival.network.SyncSize;
-import by.jackraidenph.dragonsurvival.network.SynchronizeDragonCap;
+import by.jackraidenph.dragonsurvival.nest.NestEntity;
+import by.jackraidenph.dragonsurvival.network.*;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.item.ExperienceOrbEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.SoundCategory;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.network.NetworkEvent;
@@ -67,8 +73,8 @@ public class PacketProxy {
                 		dragonStateHandler.setMovementData(syncCapabilityMovement.bodyYaw, syncCapabilityMovement.headYaw, syncCapabilityMovement.headPitch, syncCapabilityMovement.bite);
                 });
             }
-            context.setPacketHandled(true);
         }
+        context.setPacketHandled(true);
     }
 
     public DistExecutor.SafeRunnable updateSize(SyncSize syncSize, Supplier<NetworkEvent.Context> contextSupplier) {
@@ -116,8 +122,55 @@ public class PacketProxy {
                     thatPlayer.setForcedPose(null);
                     thatPlayer.refreshDimensions();
                 }
-                context.get().setPacketHandled(true);
             }
+            context.get().setPacketHandled(true);
         };
+    }
+
+    public void syncXpDevour(PacketSyncXPDevour m, Supplier<NetworkEvent.Context> supplier) {
+
+        World world = Minecraft.getInstance().level;
+        if (world != null) {
+            ExperienceOrbEntity xpOrb = (ExperienceOrbEntity) (world.getEntity(m.xp));
+            MagicalPredatorEntity entity = (MagicalPredatorEntity) world.getEntity(m.entity);
+            if (xpOrb != null && entity != null) {
+                entity.size += xpOrb.getValue() / 100.0F;
+                entity.size = MathHelper.clamp(entity.size, 0.95F, 1.95F);
+                world.addParticle(ParticleTypes.SMOKE, xpOrb.getX(), xpOrb.getY(), xpOrb.getZ(), 0, world.getRandom().nextFloat() / 12.5f, 0);
+                xpOrb.remove();
+                supplier.get().setPacketHandled(true);
+            }
+        }
+    }
+
+    public void syncPredatorStats(PacketSyncPredatorStats m, Supplier<NetworkEvent.Context> supplier) {
+
+        World world = Minecraft.getInstance().level;
+        if (world != null) {
+            Entity entity = world.getEntity(m.id);
+            if (entity != null) {
+                ((MagicalPredatorEntity) entity).size = m.size;
+                ((MagicalPredatorEntity) entity).type = m.type;
+                supplier.get().setPacketHandled(true);
+            }
+        }
+    }
+
+    public void syncNest(SynchronizeNest synchronizeNest, Supplier<NetworkEvent.Context> contextSupplier) {
+        PlayerEntity player = Minecraft.getInstance().player;
+        ClientWorld world = Minecraft.getInstance().level;
+        TileEntity entity = world.getBlockEntity(synchronizeNest.pos);
+        if (entity instanceof NestEntity) {
+            NestEntity nestEntity = (NestEntity) entity;
+            nestEntity.energy = synchronizeNest.health;
+            nestEntity.damageCooldown = synchronizeNest.cooldown;
+            nestEntity.setChanged();
+            if (nestEntity.energy <= 0) {
+                world.playSound(player, synchronizeNest.pos, SoundEvents.METAL_BREAK, SoundCategory.BLOCKS, 1, 1);
+            } else {
+                world.playSound(player, synchronizeNest.pos, SoundEvents.SHIELD_BLOCK, SoundCategory.BLOCKS, 1, 1);
+            }
+            contextSupplier.get().setPacketHandled(true);
+        }
     }
 }
