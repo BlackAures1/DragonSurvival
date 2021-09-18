3f59893ab36ee693d4443daf5deca355012e9451
Alexiy <alexiy.ov@gmail.com>
2021-09-18 18:20:45 +0300

    Adjust damage formula, and make damage non-lethal

--- src/main/java/by/jackraidenph/dragonsurvival/handlers/ServerFlightHandler.java
+++ src/main/java/by/jackraidenph/dragonsurvival/handlers/ServerFlightHandler.java
@@ -4,6 +4,7 @@ import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
 import by.jackraidenph.dragonsurvival.config.ConfigHandler;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.util.DamageSource;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -31,8 +32,10 @@ public class ServerFlightHandler {
             } else if (ConfigHandler.SERVER.enableFlightFallDamage.get()) {
                 DragonStateProvider.getCap(livingEntity).ifPresent(dragonStateHandler -> {
                     if (dragonStateHandler.isDragon() && DragonSizeHandler.wingsStatusServer.containsKey(livingEntity.getId()) && DragonSizeHandler.wingsStatusServer.get(livingEntity.getId())) {
-                        if (flightSpeed > 0.3) {
-                            event.setAmount((float) (flightSpeed * 5 * dragonStateHandler.getSize() / 20));
+                        if (flightSpeed > 0.03) {
+                            double damage = flightSpeed * 70 * dragonStateHandler.getSize() / 20;
+                            damage = MathHelper.clamp(damage, 0, livingEntity.getHealth() - 1);
+                            event.setAmount((float) (damage));
                         } else {
                             event.setCanceled(true);
                         }
