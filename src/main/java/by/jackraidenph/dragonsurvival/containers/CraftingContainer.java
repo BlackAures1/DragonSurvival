c57cd953ea1c9813ae81445d9f58baddca551725
Alexiy <alexiy.ov@gmail.com>
2021-09-19 10:27:45 +0300

    Replace reflection

--- src/main/java/by/jackraidenph/dragonsurvival/containers/CraftingContainer.java
+++ src/main/java/by/jackraidenph/dragonsurvival/containers/CraftingContainer.java
@@ -3,21 +3,15 @@ package by.jackraidenph.dragonsurvival.containers;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.entity.player.ServerPlayerEntity;
-import net.minecraft.inventory.CraftResultInventory;
-import net.minecraft.inventory.CraftingInventory;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.inventory.container.WorkbenchContainer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.ICraftingRecipe;
 import net.minecraft.item.crafting.IRecipeType;
 import net.minecraft.network.play.server.SSetSlotPacket;
-import net.minecraft.util.IWorldPosCallable;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
-import java.lang.reflect.Field;
 import java.util.Optional;
-import java.util.function.BiFunction;
 
 public class CraftingContainer extends WorkbenchContainer {
     World world;
@@ -36,44 +30,26 @@ public class CraftingContainer extends WorkbenchContainer {
     @Override
     public void slotsChanged(IInventory p_75130_1_) {
         if (!world.isClientSide) {
-            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity)playerEntity;
+            ServerPlayerEntity serverplayerentity = (ServerPlayerEntity) playerEntity;
             ItemStack itemstack = ItemStack.EMPTY;
-            Field fieldCrafing=WorkbenchContainer.class.getDeclaredFields()[0];
-            fieldCrafing.setAccessible(true);
-            Field fieldResultinv=WorkbenchContainer.class.getDeclaredFields()[1];
-            fieldResultinv.setAccessible(true);
-            try {
-                if(fieldCrafing.getType()==CraftingInventory.class && fieldResultinv.getType()==CraftResultInventory.class) {
-                    CraftingInventory craftingInventory = (CraftingInventory) fieldCrafing.get(this);
-                    CraftResultInventory resultInventory = (CraftResultInventory) fieldResultinv.get(this);
-                    Optional<ICraftingRecipe> optional = world.getServer().getRecipeManager().getRecipeFor(IRecipeType.CRAFTING, craftingInventory, world);
-                    if (optional.isPresent()) {
-                        ICraftingRecipe icraftingrecipe = optional.get();
-                        if (resultInventory.setRecipeUsed(world, serverplayerentity, icraftingrecipe)) {
-                            itemstack = icraftingrecipe.assemble(craftingInventory);
-                        }
-                    }
-
-                    resultInventory.setItem(0, itemstack);
-                    serverplayerentity.connection.send(new SSetSlotPacket(containerId, 0, itemstack));
+            Optional<ICraftingRecipe> optional = world.getServer().getRecipeManager().getRecipeFor(IRecipeType.CRAFTING, craftSlots, world);
+            if (optional.isPresent()) {
+                ICraftingRecipe icraftingrecipe = optional.get();
+                if (resultSlots.setRecipeUsed(world, serverplayerentity, icraftingrecipe)) {
+                    itemstack = icraftingrecipe.assemble(craftSlots);
                 }
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
             }
+
+            resultSlots.setItem(0, itemstack);
+            serverplayerentity.connection.send(new SSetSlotPacket(containerId, 0, itemstack));
+
         }
     }
 
     @Override
     public void removed(PlayerEntity playerEntity) {
         super.removed(playerEntity);
-        Field fieldCrafting=WorkbenchContainer.class.getDeclaredFields()[0];
-        fieldCrafting.setAccessible(true);
-        if(fieldCrafting.getType()==CraftingInventory.class) {
-            try {
-                clearContainer(playerEntity,world, (IInventory) fieldCrafting.get(this));
-            } catch (IllegalAccessException e) {
-                e.printStackTrace();
-            }
-        }
+        clearContainer(playerEntity, world, craftSlots);
+
     }
 }
