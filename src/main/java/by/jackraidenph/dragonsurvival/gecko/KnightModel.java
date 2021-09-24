a6e507667750894523d8ff9c9655e55b0a407ab4
alexiy <alexiy.ov@gmail.com>
2021-09-24 14:18:26 +0300

    Model

---
+++ src/main/java/by/jackraidenph/dragonsurvival/gecko/KnightModel.java
@@ -0,0 +1,22 @@
+package by.jackraidenph.dragonsurvival.gecko;
+
+import by.jackraidenph.dragonsurvival.DragonSurvivalMod;
+import net.minecraft.util.ResourceLocation;
+import software.bernie.geckolib3.model.AnimatedGeoModel;
+
+public class KnightModel extends AnimatedGeoModel<Knight> {
+    @Override
+    public ResourceLocation getModelLocation(Knight object) {
+        return new ResourceLocation(DragonSurvivalMod.MODID, "geo/dragon_knight.geo.json");
+    }
+
+    @Override
+    public ResourceLocation getTextureLocation(Knight object) {
+        return new ResourceLocation(DragonSurvivalMod.MODID, "textures/dragon_knight_black.png");
+    }
+
+    @Override
+    public ResourceLocation getAnimationFileLocation(Knight animatable) {
+        return new ResourceLocation(DragonSurvivalMod.MODID, "animations/dragon_knight.animations.json");
+    }
+}
