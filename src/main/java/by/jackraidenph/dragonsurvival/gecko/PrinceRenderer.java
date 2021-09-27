350aba9077ce26422779107aadcf654dd63e321f
alexiy <alexiy.ov@gmail.com>
2021-09-27 14:15:55 +0300

    Add main item rendering

--- src/main/java/by/jackraidenph/dragonsurvival/gecko/PrinceRenderer.java
+++ src/main/java/by/jackraidenph/dragonsurvival/gecko/PrinceRenderer.java
@@ -6,9 +6,11 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import net.minecraft.util.math.vector.Vector3f;
 import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
+import software.bernie.geckolib3.util.RenderUtils;
 
 public class PrinceRenderer extends GeoEntityRenderer<Prince> {
     public PrinceRenderer(EntityRendererManager renderManager, AnimatedGeoModel<Prince> modelProvider) {
@@ -17,8 +19,11 @@ public class PrinceRenderer extends GeoEntityRenderer<Prince> {
 
     @Override
     public void renderRecursively(GeoBone bone, MatrixStack stack, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
-        if (bone.getName().equals("left_arm")) {
+        if (bone.getName().equals("left_item")) {
             stack.pushPose();
+            RenderUtils.moveToPivot(bone, stack);
+            stack.mulPose(Vector3f.ZP.rotationDegrees(180));
+            stack.translate(0.0, -0.3, -0.5);
             Minecraft.getInstance().getItemRenderer().renderStatic(mainHand, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, packedLightIn, packedOverlayIn, stack, rtb);
             stack.popPose();
             bufferIn = rtb.getBuffer(RenderType.entityTranslucent(whTexture));
