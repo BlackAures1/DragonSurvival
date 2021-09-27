0c15a1b3f5a5f74a34cfc577a10b9c659c110665
alexiy <alexiy.ov@gmail.com>
2021-09-27 12:43:58 +0300

    Try render main item

--- src/main/java/by/jackraidenph/dragonsurvival/gecko/PrinceRenderer.java
+++ src/main/java/by/jackraidenph/dragonsurvival/gecko/PrinceRenderer.java
@@ -1,11 +1,28 @@
 package by.jackraidenph.dragonsurvival.gecko;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.vertex.IVertexBuilder;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.model.ItemCameraTransforms;
+import software.bernie.geckolib3.geo.render.built.GeoBone;
 import software.bernie.geckolib3.model.AnimatedGeoModel;
 import software.bernie.geckolib3.renderers.geo.GeoEntityRenderer;
 
 public class PrinceRenderer extends GeoEntityRenderer<Prince> {
     public PrinceRenderer(EntityRendererManager renderManager, AnimatedGeoModel<Prince> modelProvider) {
         super(renderManager, modelProvider);
+    }
+
+    @Override
+    public void renderRecursively(GeoBone bone, MatrixStack stack, IVertexBuilder bufferIn, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
+        if (bone.getName().equals("left_arm")) {
+            stack.pushPose();
+            Minecraft.getInstance().getItemRenderer().renderStatic(mainHand, ItemCameraTransforms.TransformType.THIRD_PERSON_LEFT_HAND, packedLightIn, packedOverlayIn, stack, rtb);
+            stack.popPose();
+            bufferIn = rtb.getBuffer(RenderType.entityTranslucent(whTexture));
+        }
+        super.renderRecursively(bone, stack, bufferIn, packedLightIn, packedOverlayIn, red, green, blue, alpha);
     }
 }
