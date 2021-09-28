19f3fdee776dfc97974a72c62be6e15c7a9f52a2
alexiy <alexiy.ov@gmail.com>
2021-09-28 15:52:26 +0300

    Deprecate

--- src/main/java/by/jackraidenph/dragonsurvival/mixins/MixinClientPlayNetHandler.java
+++ src/main/java/by/jackraidenph/dragonsurvival/mixins/MixinClientPlayNetHandler.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
+@Deprecated
 @Mixin({ClientPlayNetHandler.class})
 public abstract class MixinClientPlayNetHandler {
     @Inject(at = {@At("HEAD")}, method = {"(Lnet/minecraft/network/play/server/SSpawnObjectPacket;)V"})
