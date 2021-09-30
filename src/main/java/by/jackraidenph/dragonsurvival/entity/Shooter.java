05948ae81a189ad9a4db33633ff3f665c53f19bb
alexiy <alexiy.ov@gmail.com>
2021-09-30 19:55:15 +0300

    Crossbow usage boilerplate

--- src/main/java/by/jackraidenph/dragonsurvival/entity/Shooter.java
+++ src/main/java/by/jackraidenph/dragonsurvival/entity/Shooter.java
@@ -5,20 +5,29 @@ import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
 import by.jackraidenph.dragonsurvival.config.ConfigHandler;
 import by.jackraidenph.dragonsurvival.gecko.Knight;
 import by.jackraidenph.dragonsurvival.goals.AlertExceptHunters;
+import by.jackraidenph.dragonsurvival.goals.CrossbowAttackGoal;
 import net.minecraft.entity.CreatureEntity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ICrossbowUser;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.monster.AbstractIllagerEntity;
+import net.minecraft.entity.monster.PillagerEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.projectile.ProjectileEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.World;
 
 public class Shooter extends Hunter implements ICrossbowUser {
+    private static final DataParameter<Boolean> IS_CHARGING_CROSSBOW = EntityDataManager.defineId(PillagerEntity.class, DataSerializers.BOOLEAN);
+
     protected int bolasCooldown = Functions.secondsToTicks(30);
+
     public Shooter(EntityType<? extends CreatureEntity> entityType, World world) {
         super(entityType, world);
     }
@@ -26,27 +35,28 @@ public class Shooter extends Hunter implements ICrossbowUser {
     @Override
     protected void registerGoals() {
         super.registerGoals();
+        goalSelector.addGoal(3, new CrossbowAttackGoal<>(this, 1.0D, 8.0F));
         goalSelector.addGoal(8, new AlertExceptHunters<>(this, HunterHound.class, Knight.class, Squire.class));
     }
 
     @Override
     public void setChargingCrossbow(boolean p_213671_1_) {
-
+        entityData.set(IS_CHARGING_CROSSBOW, p_213671_1_);
     }
 
     @Override
     public void shootCrossbowProjectile(LivingEntity p_230284_1_, ItemStack p_230284_2_, ProjectileEntity p_230284_3_, float p_230284_4_) {
-
+        shootCrossbowProjectile(this, p_230284_1_, p_230284_3_, p_230284_4_, 1.6F);
     }
 
     @Override
     public void onCrossbowAttackPerformed() {
-
+        noActionTime = 0;
     }
 
     @Override
     public void performRangedAttack(LivingEntity p_82196_1_, float p_82196_2_) {
-
+        performCrossbowAttack(this, 1.6F);
     }
 
     public void tick() {
@@ -83,5 +93,24 @@ public class Shooter extends Hunter implements ICrossbowUser {
     public void readAdditionalSaveData(CompoundNBT compoundNBT) {
         super.readAdditionalSaveData(compoundNBT);
         this.bolasCooldown = compoundNBT.getInt("Bolas cooldown");
+    }
+
+    protected void defineSynchedData() {
+        super.defineSynchedData();
+        this.entityData.define(IS_CHARGING_CROSSBOW, false);
+    }
+
+    public boolean isChargingCrossbow() {
+        return this.entityData.get(IS_CHARGING_CROSSBOW);
+    }
+
+    public AbstractIllagerEntity.ArmPose getArmPose() {
+        if (this.isChargingCrossbow()) {
+            return AbstractIllagerEntity.ArmPose.CROSSBOW_CHARGE;
+        } else if (this.isHolding(item -> item instanceof net.minecraft.item.CrossbowItem)) {
+            return AbstractIllagerEntity.ArmPose.CROSSBOW_HOLD;
+        } else {
+            return this.isAggressive() ? AbstractIllagerEntity.ArmPose.ATTACKING : AbstractIllagerEntity.ArmPose.NEUTRAL;
+        }
     }
 }
