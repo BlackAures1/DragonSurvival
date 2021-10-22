262550c70e2ff9ce445c97f6c3b8619c399178cb
alexiy <alexiy.ov@gmail.com>
2021-10-22 19:30:06 +0300

    Setting beacon type

--- src/main/java/by/jackraidenph/dragonsurvival/blocks/DragonBeacon.java
+++ src/main/java/by/jackraidenph/dragonsurvival/blocks/DragonBeacon.java
@@ -1,14 +1,23 @@
 package by.jackraidenph.dragonsurvival.blocks;
 
+import by.jackraidenph.dragonsurvival.registration.TileEntityTypesInit;
+import by.jackraidenph.dragonsurvival.tiles.DragonBeaconEntity;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
 
 public class DragonBeacon extends Block {
     public DragonBeacon(Properties p_i48440_1_) {
@@ -16,8 +25,35 @@ public class DragonBeacon extends Block {
     }
 
     @Override
-    public ActionResultType use(BlockState p_225533_1_, World p_225533_2_, BlockPos p_225533_3_, PlayerEntity playerEntity, Hand p_225533_5_, BlockRayTraceResult p_225533_6_) {
+    public ActionResultType use(BlockState p_225533_1_, World world, BlockPos pos, PlayerEntity playerEntity, Hand hand, BlockRayTraceResult p_225533_6_) {
+        ItemStack itemStack = playerEntity.getItemInHand(hand);
+        Item item = itemStack.getItem();
+        DragonBeaconEntity dragonBeaconEntity = (DragonBeaconEntity) world.getBlockEntity(pos);
+        if (item == Items.GOLD_BLOCK) {
+            dragonBeaconEntity.type = DragonBeaconEntity.Type.PEACE;
+            itemStack.shrink(1);
+            return ActionResultType.SUCCESS;
+        } else if (item == Items.DIAMOND_BLOCK) {
+            dragonBeaconEntity.type = DragonBeaconEntity.Type.MAGIC;
+            itemStack.shrink(1);
+            return ActionResultType.SUCCESS;
+        } else if (item == Items.NETHERITE_INGOT) {
+            dragonBeaconEntity.type = DragonBeaconEntity.Type.VETO;
+            itemStack.shrink(1);
+            return ActionResultType.SUCCESS;
+        }
         playerEntity.hurt(DamageSource.GENERIC, 1);
         return ActionResultType.SUCCESS;
+    }
+
+    @Override
+    public boolean hasTileEntity(BlockState state) {
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public TileEntity createTileEntity(BlockState state, IBlockReader world) {
+        return TileEntityTypesInit.dragonBeacon.create();
     }
 }
