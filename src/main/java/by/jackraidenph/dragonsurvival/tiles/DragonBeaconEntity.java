a51da4d36c1d14b5a71ba9b4d18d261c896acbff
alexiy <alexiy.ov@gmail.com>
2021-10-25 12:36:51 +0300

    Apply effects

--- src/main/java/by/jackraidenph/dragonsurvival/tiles/DragonBeaconEntity.java
+++ src/main/java/by/jackraidenph/dragonsurvival/tiles/DragonBeaconEntity.java
@@ -1,9 +1,18 @@
 package by.jackraidenph.dragonsurvival.tiles;
 
+import by.jackraidenph.dragonsurvival.Functions;
+import by.jackraidenph.dragonsurvival.capability.DragonStateProvider;
+import by.jackraidenph.dragonsurvival.registration.BlockInit;
+import by.jackraidenph.dragonsurvival.registration.DragonEffects;
 import by.jackraidenph.dragonsurvival.registration.TileEntityTypesInit;
+import by.jackraidenph.dragonsurvival.util.EffectInstance2;
 import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+
+import java.util.List;
 
 public class DragonBeaconEntity extends BaseBlockEntity implements ITickableTileEntity {
     public Type type = Type.NONE;
@@ -21,7 +30,21 @@ public class DragonBeaconEntity extends BaseBlockEntity implements ITickableTile
 
     @Override
     public void tick() {
-
+        BlockState below = level.getBlockState(getBlockPos().below());
+        if (below.getBlock() == BlockInit.dragonMemoryBlock && type != Type.NONE) {
+            List<PlayerEntity> dragons = level.getEntitiesOfClass(PlayerEntity.class, new AxisAlignedBB(getBlockPos()).inflate(50).expandTowards(0, level.getMaxBuildHeight(), 0), DragonStateProvider::isDragon);
+            switch (type) {
+                case PEACE:
+                    dragons.forEach(playerEntity -> playerEntity.addEffect(new EffectInstance2(DragonEffects.PEACE, Functions.secondsToTicks(20))));
+                    break;
+                case MAGIC:
+                    dragons.forEach(playerEntity -> playerEntity.addEffect(new EffectInstance2(DragonEffects.MAGIC, Functions.secondsToTicks(20))));
+                    break;
+                case VETO:
+                    dragons.forEach(playerEntity -> playerEntity.addEffect(new EffectInstance2(DragonEffects.VETO, Functions.secondsToTicks(20))));
+                    break;
+            }
+        }
     }
 
     @Override
