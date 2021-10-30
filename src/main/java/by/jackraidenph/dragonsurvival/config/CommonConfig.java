ad4a5f456c8e122f6c9836ca904913f588a4028f
alexiy <alexiy.ov@gmail.com>
2021-10-30 04:12:56 +0300

    Fix tree

--- src/main/java/by/jackraidenph/dragonsurvival/config/CommonConfig.java
+++ src/main/java/by/jackraidenph/dragonsurvival/config/CommonConfig.java
@@ -154,7 +154,7 @@ public class CommonConfig {
 		princeDamage = builder.comment("Prince base damage").defineInRange("princeDamage", 1d, 1d, 20d);
 		princeSpeed = builder.comment("Prince speed").defineInRange("princeSpeed", 0.3d, 0.2d, 0.6d);
 		princeArmor = builder.comment("Prince armor").defineInRange("princeArmor", 6d, 0d, 20d);
-		builder.pop();
+		builder.pop(2);
 		builder.push("dragonBeacons");
 		secondsOfBeaconEffect = builder.comment("Duration of effect given by beacon constantly in seconds").defineInRange("constantEffect", 20, 1, 60 * 60);
 		minutesOfDragonEffect = builder.comment("Duration of effect given in exchange for experience in minutes").defineInRange("temporaryEffect", 10, 1, 60 * 2);
